528f8320c798546a5a56453276f3af38e94e3569
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-20 22:22:16 +0800

    add GTA dataset

---
+++ LeReS/Train/scripts/train_gta_demo.sh
@@ -0,0 +1,17 @@
+export PYTHONPATH=../../Train:${PYTHONPATH}
+
+python ../tools/train.py \
+--dataset gta \
+--dataroot /share/torch_ds/GTA-IM \
+--backbone resnet50 \
+--dataset_list demo \
+--batchsize 2 \
+--base_lr 0.001 \
+--use_tfboard \
+--thread 4 \
+--loss_mode _ranking-edge_pairwise-normal-regress-edge_msgil-normal_meanstd-tanh_pairwise-normal-regress-plane_ranking-edge-auxi_meanstd-tanh-auxi_ \
+--epoch 2 \
+--lr_scheduler_multiepochs 1 \
+--val_step 50 \
+--snapshot_iters 50 \
+--log_interval 5 
