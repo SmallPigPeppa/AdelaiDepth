73a702aea9b36c15662dc3691ca73796547da4f4
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-21 20:53:22 +0800

    add GTA dataset

--- LeReS/Train/scripts/train_gta_demo.sh
+++ LeReS/Train/scripts/train_gta_demo.sh
@@ -13,6 +13,6 @@ python ../tools/train.py \
 --loss_mode _ranking-edge_pairwise-normal-regress-edge_msgil-normal_meanstd-tanh_pairwise-normal-regress-plane_ranking-edge-auxi_meanstd-tanh-auxi_ \
 --epoch 2 \
 --lr_scheduler_multiepochs 1 \
---val_step 50 \
+--val_step 10000 \
 --snapshot_iters 50 \
 --log_interval 5 
