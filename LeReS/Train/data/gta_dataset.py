11b621cecef68eb43968211e7e301debe5f6f0d5
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-21 20:31:14 +0800

    add GTA dataset

--- LeReS/Train/data/gta_dataset.py
+++ LeReS/Train/data/gta_dataset.py
@@ -116,8 +116,7 @@ class GTADataset(Dataset):
         rgb = cv2.imread(rgb_path)[:, :, ::-1]  # rgb, H*W*C
         joints_2d = self.info_npz['joints_2d'][anno_index]
         joints_3d_cam = self.info_npz['joints_3d_cam'][anno_index]
-        # joints_3d_world = self.info_npz['joints_3d_world'][anno_index]
-        joints_3d_world = None
+        joints_3d_world = self.info_npz['joints_3d_world'][anno_index]
         world2cam_trans = self.info_npz['world2cam_trans'][anno_index]
         intrinsics = self.info_npz['intrinsics'][anno_index]
         focal_length = (intrinsics[0][0]).astype(np.float32)
