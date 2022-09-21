174bc38787d125552eea7f649380981009becb1a
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-21 11:00:20 +0800

    add GTA dataset

--- LeReS/Train/data/gta_dataset.py
+++ LeReS/Train/data/gta_dataset.py
@@ -21,6 +21,7 @@ class GTADataset(Dataset):
         self.dataset_name = dataset_name
         self.rgb_paths, self.depth_paths, self.mask_paths, self.cam_near_clips, self.cam_far_clips, self.info_pkl, self.info_npz = self.getData()
         self.data_size = len(self.info_pkl)
+        self.curriculum_list = list(np.random.choice(self.data_size, self.data_size, replace=False))
 
     def getData(self):
         data_path = os.path.join(self.root,self.dataset_name)
